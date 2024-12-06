0f86b4f8deb0ee69759a2e226aad0636b33caff1
yuri_sn <yuri.snitko@gmail.com>
2024-12-07 00:17:50 +0300

    add changes

--- upload-loader/src/components/dragAndDrop.tsx
+++ upload-loader/src/components/dragAndDrop.tsx
@@ -2,14 +2,28 @@ import Image from 'next/image';
 
 export default function DragAndDrop() {
   return (
-    <div>
-      <Image src={'/Link.svg'} width={40} height={40} alt="link" />
+    <form
+      action=""
+      className="flex flex-col justify-center items-center size-full gap-3"
+    >
+      <Image src={'/exit.svg'} width={40} height={40} alt="link" />
       <div>
-        <label htmlFor="">
-          Drag & Drop a file or <input type="file" name="" id="" />
-        </label>
+        <p>
+          Drag & Drop a file or &nbsp;
+          <label htmlFor="browse" className="pointer text-clr_1">
+            browse files
+            <input
+              type="file"
+              name=""
+              id="browse"
+              className=" opacity-0 absolute -z-[1]"
+              accept=".jpg, .png, .jpeg"
+            />
+          </label>
+        </p>
       </div>
+
       <p>JPG, PNG, GIF - Max file size 2MB</p>
-    </div>
+    </form>
   );
 }
